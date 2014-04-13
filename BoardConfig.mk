671fed71f4fbf7e4227eb2bfea53647503353659
slayher <layhertony@gmail.com>
2014-04-12 22:11:30 -0400

    HLTE: Clean up of BoardConfig.
     Remove unused Defines.
     Remove items defined in inherited configs.
     Re-arrange defines to group common ones.
    
    Change-Id: Ie14df28368d964e0a59c091bf28b3ee547f4435a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,46 +25,43 @@
 
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/hlte/include
 
-
-# overrides  msm8960
+# Hlte specific and MSM8974 overrides of msm8960-common
 TARGET_BOARD_PLATFORM := msm8974
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
 TARGET_BOOTLOADER_BOARD_NAME := MSM8974
+WIFI_DRIVER_FW_PATH_P2P := 
 
 # Kernel Configs
 TARGET_KERNEL_SOURCE := kernel/samsung/hlte
 TARGET_KERNEL_CONFIG := msm8974_sec_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_hlte_eur_defconfig
 
-# Kernel
+# Boot Image Defines
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
 BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/hlte/mkbootimg.mk
 
-# Graphics
-BOARD_EGL_CFG := device/samsung/hlte/egl.cfg
-
-# Recovery
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+# Recovery Defines
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/hlte/recovery/recovery_keys.c
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_RECOVERY_SWIPE := true
 TARGET_RECOVERY_FSTAB := device/samsung/hlte/rootdir/etc/fstab.qcom
 
+# Filesystem Defines
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 11534336
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 13631488
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 28651290624
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-BOARD_RECOVERY_SWIPE := true
 
 # bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/hlte/bluetooth
@@ -74,34 +71,19 @@ BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 # NFC
 BOARD_NFC_HAL_SUFFIX := msm8974
 
-# Samsung's nonstandard csd-client
-BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
-
-# QCOM support
+# QCOM  Hardware Defines
 BOARD_USES_QCOM_HARDWARE := true
+TARGET_USES_QCOM_BSP := true
 TARGET_QCOM_MEDIA_VARIANT := caf-new
 TARGET_QCOM_DISPLAY_VARIANT := caf-new
-BOARD_USES_LEGACY_ALSA_AUDIO := 
-TARGET_QCOM_AUDIO_VARIANT := caf
-TARGET_USES_QCOM_BSP := true
 
 # Audio settings
+BOARD_USES_LEGACY_ALSA_AUDIO := 
 BOARD_USES_CUSTOM_AUDIO_PLATFORM_PATH := device/samsung/hlte/audio/platform
+BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
 AUDIO_FEATURE_DISABLED_MULTI_VOICE_SESSIONS := true
 AUDIO_FEATURE_DISABLED_FM := true
 AUDIO_FEATURE_DISABLED_ANC_HEADSET := true
-#AUDIO_FEATURE_DISABLED_SSR := true
-#AUDIO_FEATURE_DISABLED_INCALL_MUSIC := true
-#AUDIO_FEATURE_DISABLED_SPKR_PROTECTION := true
-#AUDIO_FEATURE_DISABLED_DS1_DOLBY_DDP := true
-
-WIFI_DRIVER_FW_PATH_P2P     := 
-
-# Don't use qcom camera HAL
-#USE_DEVICE_SPECIFIC_CAMERA := true
-
-# Build lights 
-TARGET_PROVIDES_LIBLIGHT := true
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
@@ -110,6 +92,9 @@ BOARD_USES_QC_TIME_SERVICES := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+
+# Graphics
+BOARD_EGL_CFG := device/samsung/hlte/egl.cfg
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
@@ -123,8 +108,6 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := hltexx,hltespr,hltetmo,SM-N900T,hltecan,hlteatt,hltevzw,hlte
-
-TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_hlte_eur_defconfig
 
 # PowerHAL
 TARGET_POWERHAL_VARIANT := qcom
