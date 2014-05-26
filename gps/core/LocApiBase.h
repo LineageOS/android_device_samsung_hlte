aaf74719e5c7fcb3f1f391f7b42bca9d05ad25fc
Shareef Ali <shareefalis@gmail.com>
2014-05-26 12:35:44 -0400

    gps hal: port of mkasick's patch for privacy settings
    https://github.com/CyanogenMod/android_device_samsung_hlte/commit/2713a295f8a5f104d0be4a188394e72a4fd6b728
    Change-Id: I8ebefcbcfc48b30abe61c3aff7bc318b7bfe3211

--- gps/core/LocApiBase.h
+++ gps/core/LocApiBase.h
@@ -209,6 +209,20 @@ public:
       -1 on failure
      */
     virtual int getGpsLock(void);
+private:
+    // Pad out virtual method table so that the setPrivacy entry corresponds to
+    // the same as LocApiV02Adapter.
+    inline virtual void unknownVirtualMethod26() {}
+    inline virtual void unknownVirtualMethod27() {}
+    inline virtual void unknownVirtualMethod28() {}
+    inline virtual void unknownVirtualMethod29() {}
+    inline virtual void unknownVirtualMethod30() {}
+    inline virtual void unknownVirtualMethod31() {}
+    
+public:
+    inline virtual enum loc_api_adapter_err
+    setPrivacy(int8_t privacy_setting)
+    {LOC_LOGW("%s: default implementation invoked", __func__); return LOC_API_ADAPTER_ERR_SUCCESS;}
 };
 
 typedef LocApiBase* (getLocApi_t)(const MsgTask* msgTask,
--- gps/loc_api/libloc_api_50001/loc_eng.cpp
+++ gps/loc_api/libloc_api_50001/loc_eng.cpp
@@ -179,7 +179,8 @@ static void loc_eng_process_conn_request(loc_eng_data_s_type &loc_eng_data,
 static void loc_eng_agps_close_status(loc_eng_data_s_type &loc_eng_data, int is_succ);
 static void loc_eng_handle_engine_down(loc_eng_data_s_type &loc_eng_data) ;
 static void loc_eng_handle_engine_up(loc_eng_data_s_type &loc_eng_data) ;
-
+static int loc_eng_set_privacy(loc_eng_data_s_type &loc_eng_data,
+                               int8_t privacy_setting);
 static int loc_eng_start_handler(loc_eng_data_s_type &loc_eng_data);
 static int loc_eng_stop_handler(loc_eng_data_s_type &loc_eng_data);
 static int loc_eng_get_zpp_handler(loc_eng_data_s_type &loc_eng_data);
@@ -1492,7 +1493,11 @@ int loc_eng_init(loc_eng_data_s_type &loc_eng_data, LocCallbacks* callbacks,
         EXIT_LOG(%d, ret_val);
         return ret_val;
     }
-
+    if (NULL != loc_eng_data.context) {
+        // Current loc_eng_cleanup keeps context initialized, so must enable
+        // here too.
+        loc_eng_set_privacy(loc_eng_data, 1);
+    }
     STATE_CHECK((NULL == loc_eng_data.adapter),
                 "instance already initialized", return 0);
 
@@ -1550,7 +1555,6 @@ static int loc_eng_reinit(loc_eng_data_s_type &loc_eng_data)
 
     if (LOC_API_ADAPTER_ERR_SUCCESS == ret_val) {
         LOC_LOGD("loc_eng_reinit reinit() successful");
-
         LocEngAdapter* adapter = loc_eng_data.adapter;
         adapter->sendMsg(new LocEngSuplVer(adapter, gps_conf.SUPL_VER));
         adapter->sendMsg(new LocEngLppConfig(adapter, gps_conf.LPP_PROFILE));
@@ -1591,7 +1595,9 @@ static int loc_eng_reinit(loc_eng_data_s_type &loc_eng_data)
 
         adapter->sendMsg(new LocEngEnableData(adapter, NULL, 0, (agpsStatus ? 1:0)));
     }
-
+    if (LOC_API_ADAPTER_ERR_SUCCESS == ret_val) {
+        loc_eng_set_privacy(loc_eng_data, 1);
+    }
     EXIT_LOG(%d, ret_val);
     return ret_val;
 }
@@ -1636,6 +1642,7 @@ void loc_eng_cleanup(loc_eng_data_s_type &loc_eng_data)
         LOC_LOGD("loc_eng_cleanup: fix not stopped. stop it now.");
         loc_eng_stop(loc_eng_data);
     }
+    loc_eng_set_privacy(loc_eng_data, 0);
 
 #if 0 // can't afford to actually clean up, for many reason.
 
@@ -2594,6 +2601,48 @@ int loc_eng_read_config(void)
       LOC_LOGV("GPS Config file has already been read\n");
     }
 
+    EXIT_LOG(%d, 0);
+    return 0;
+}
+
+/*===========================================================================
+ FUNCTION loc_eng_set_privacy
+ 
+ DESCRIPTION
+ Sets the privacy lock setting (1. GPS on, 0. GPS off).
+ 
+ DEPENDENCIES
+ ro.gps.set_privacy system property must be "1" to set privacy lock.
+ 
+ RETURN VALUE
+ 0: success
+ 
+ SIDE EFFECTS
+ N/A
+ 
+ ===========================================================================*/
+static int loc_eng_set_privacy(loc_eng_data_s_type &loc_eng_data,
+                               int8_t privacy_setting)
+{
+    //static const char SET_PRIVACY_PROP[] = "ro.gps.set_privacy";
+    // char value[PROPERTY_VALUE_MAX];
+    
+    ENTRY_LOG();
+    INIT_CHECK(loc_eng_data.context, return -1);
+    // property_get(SET_PRIVACY_PROP, value, "");
+    
+    // if (strcmp(value, "1") == 0) {
+    LOC_LOGI("%s: Dispatch setPrivacy", __func__);
+    
+    loc_eng_msg_privacy *msg(
+                             new loc_eng_msg_privacy(&loc_eng_data, privacy_setting));
+    msg_q_snd((void*)((LocEngContext*)(loc_eng_data.context))->deferred_q,
+              msg, loc_eng_free_msg);
+    //} else {
+    // LOC_LOGD("%s: Drop setPrivacy, %s=\"%s\" != \"1\".\n", __func__,
+    // SET_PRIVACY_PROP, value);
+    // }
+    
     EXIT_LOG(%d, 0);
     return 0;
 }
