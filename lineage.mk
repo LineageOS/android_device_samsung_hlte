5733e2172903f4d7e78742c36f0e0ddba1483261
trader418 <trader418@googlemail.com>
2017-03-08 01:55:43 +0000

    Rebrand to lineage

--- lineage.mk
+++ lineage.mk
@@ -4,4 +4,4 @@ $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := hlte
-PRODUCT_NAME := cm_hlte
+PRODUCT_NAME := lineage_hlte
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_hlte-eng
+add_lunch_combo lineage_hlte-eng
