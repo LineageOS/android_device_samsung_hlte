57998bf9959c7a73365c66ae7e6b901a0b6412bb
Kevin F. Haggerty <haggertk@lineageos.org>
2017-08-30 06:45:00 -0600

    hlte: Inherit from vendor/lineage
    Change-Id: Iadc568801de6b3b135310441d20323b9144b7e0a

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,22 @@
-$(call inherit-product, device/samsung/hlte/full_hlte.mk)
+# Copyright (C) 2013-2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
 PRODUCT_DEVICE := hlte
 PRODUCT_NAME := lineage_hlte
