2dad102068f494ce74de9a770482820e1db3120b
Kevin F. Haggerty <haggertk@lineageos.org>
2020-02-28 08:01:55 -0700

    hlte: libinit: Remove vestigal ISMATCH definition
    * This hasn't been used since we move to using std::string
    
    Change-Id: I4ff3d8b5802efdd782ba65924fb27a1b6eefb87c

--- init/init_hlte.cpp
+++ init/init_hlte.cpp
@@ -1,6 +1,6 @@
 /*
    Copyright (c) 2013, The Linux Foundation. All rights reserved.
-   Copyright (c) 2017-2018, The LineageOS Project. All rights reserved.
+   Copyright (c) 2017-2020, The LineageOS Project. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -73,8 +73,6 @@ void gsm_properties(char const *rild_lib_variant)
     property_set("ro.telephony.default_network", "9");
     property_set("telephony.lteOnGsmDevice", "1");
 }
-
-#define ISMATCH(a, b) (!strncmp((a), (b), PROP_VALUE_MAX))
 
 void init_target_properties()
 {
